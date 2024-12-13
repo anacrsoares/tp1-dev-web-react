1b6d857d42830c1e711746ea893fcec831da61ef
Ana Carolina R. Soares <86810558+anacrsoares@users.noreply.github.com>
2024-12-13 09:52:19 -0300

    Revert "Tp3"

--- src/components/MyFab/index.jsx
+++
@@ -1,6 +0,0 @@
-import { Fab, Avatar } from "@mui/material";
-import cart from "../../assets/img/cart.png";
-
-export default function MyFab({ props, children }) {
-  return <Fab {...props}>{children}</Fab>;
-}
--- src/views/Forms.tsx
+++ src/views/Forms.tsx
@@ -1,19 +1,5 @@
-import { useParams } from "react-router-dom";
-
 const Forms: React.FC = () => {
-  const params = useParams();
-
-  const getParamType = () => {
-    switch (params.type) {
-      case "1":
-        return "Sono";
-      case "2":
-        return "Amamentação";
-      default:
-        return "Fraldas";
-    }
-  };
-  return getParamType();
+  return "Forms";
 };
 
 export default Forms;
