a590b8e3fe5da0d4b17be1a8e341611864516e09
anacrsoares <anacrsoares-@outlook.comgit config --global user.email anacrsoares-@outlook.comgit config --global user.email anacrsoares-@outlook.com>
2024-12-14 01:42:32 -0300

    wip: settings

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,6 @@
 export { default as MyButton } from "./MyButton";
 export { default as Button16 } from "./Button16";
-export { default as DatePickerComponent } from "./datePicker";
+export { default as DatePicker } from "./datePicker";
 export { default as DateTimePicker } from "./dateTimePicker";
 export { default as SwitchComponent } from "./Switch";
 export { default as TabComponent } from "./Tab";
